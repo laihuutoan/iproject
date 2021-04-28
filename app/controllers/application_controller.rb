d75206de9eb31fcddd563ee9147e83694d8356e4
Toàn Lại <toan.lai@reti.vn>
2021-04-28 18:12:35 +0700

    Fix style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,4 +11,9 @@ class ApplicationController < ActionController::Base
   def authenticate_admin?
     self.class.parent == Admin
   end
+
+  def after_sign_in_path_for(resource)
+    byebug
+    session["#{resource}_return_to"] || session["user_return_to"] || stored_location_for(:user) || root_url
+  end
 end
